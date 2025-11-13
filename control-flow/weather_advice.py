ec7ce4c6d44249acb782e80bc26c4944f6bab387
peter <petercubi01@gmail.com>
2025-11-13 16:26:58 +0300

    Initial commit

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather = input("Enter the weather: ")
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+elif weather == "rainy":
+    print("Take an umbrella!")
+elif weather == "cold":
+    print("Wear a coat!")
+else:
+    print("I don't understand that weather.")
